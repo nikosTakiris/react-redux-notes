1c58d94d3a94896630abb042f850748af102b8c7
Nikos Takiris <nik_takiris@yahoo.gr>
2019-04-29 23:07:39 +0300

    Updates

---
+++ precache-manifest.391084d66f30cca31fc702cd3ec737e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fab3d06c8d0efad6732f933e97ae024",
+    "url": "/react-redux-notes/index.html"
+  },
+  {
+    "revision": "30e967a9a505b3b0f6a9",
+    "url": "/react-redux-notes/static/css/main.ea4d8cde.chunk.css"
+  },
+  {
+    "revision": "359715fbb4ffa29b326b",
+    "url": "/react-redux-notes/static/js/2.03aa6286.chunk.js"
+  },
+  {
+    "revision": "30e967a9a505b3b0f6a9",
+    "url": "/react-redux-notes/static/js/main.d25b01e2.chunk.js"
+  },
+  {
+    "revision": "86818985511d4ed52ad9",
+    "url": "/react-redux-notes/static/js/runtime~main.9ebc7661.js"
+  }
+]);
